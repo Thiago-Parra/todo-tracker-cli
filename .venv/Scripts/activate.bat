fa268cc02b484747ac865b5c9e8f176e31b802ec
Thiago Parra <thiagoparra@gmail.com>
2025-08-30 00:28:49 -0300

    funcional com novo nome de pasta

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Documentos - Google Drive\Cursos\MBA UFSCar MLP 2025\Módulo Engenharia de Software para o Big Data\Teste de Software para Sistemas que usam IA\Atividade 1\todo-tracker-cli\.venv
+set VIRTUAL_ENV=D:\Documentos - Google Drive\Cursos\MBA UFSCar MLP 2025\Módulo ESBD\Teste de Software para Sistemas que usam IA\Atividade 1\todo-tracker-cli\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
